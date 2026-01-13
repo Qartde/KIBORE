1fad5fcfb474dd7c4d2c40afff35422b08c94529
Qartde <bonifacemd2525@gmail.com>
2026-01-13 23:01:45 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Test ping.... \n\n\n RAHMANI MD IS ACTIVE \n\n\n' + "𝐀𝐋𝐖𝐀𝐘𝐒 𝐐𝐀𝐑𝐓";
+    let z = 'Test ping.... \n\n\n RAHMANI MD IS ACTIVE \n\n\n' + "*Rahmany*";
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
+    var mp4 = 'https://files.catbox.moe/4bepay.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
