995e977995ede8d5272d1adb8360d0f8fb398d21
Qartde <bonifacemd2525@gmail.com>
2026-01-17 11:46:28 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -6,12 +6,12 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"njabulo",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"rahmany",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
     if(!arg || !arg[0])
-    {return repondre("*ʜɪ ᴀᴍ ɴᴊᴀʙᴜʟᴏ ᴊʙ, ʜᴏᴡ ᴍᴀʏ ɪ ʜᴇʟᴘ ʏᴏᴜ")}
+    {return repondre("*ʜɪ ᴀᴍ 𝚁𝙰𝙷𝙼𝙰𝙽𝙸 𝙼𝙳, ʜᴏᴡ ᴍᴀʏ ɪ ʜᴇʟᴘ ʏᴏᴜ")}
     //var quest = arg.join(' ');
   try{
     
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '*powered by Njabulo*';
+      let caption = '*powered by Rahmany*';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
